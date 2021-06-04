f39f7b3d4b44fd86540d9be551d74e8f4a3e9e6b
Westflow <repllimate@gmail.com>
2021-06-05 04:01:22 +0700

    Update strings.h description

--- src/cstrings/strings.h
+++ src/cstrings/strings.h
@@ -7,60 +7,60 @@
 
 typedef struct cstring String;
 
-/* initialize container with \\0 on index 0 */
-/* @param init_cap initial capacity of container */
-/* @return empty container */
+/* initialize string with \\0 on index 0 */
+/* @param init_cap initial capacity of string */
+/* @return empty string */
 String* cs_init(size_t init_cap);
 
-/* create container from raw string */
-/* @param raw char* of char[] */
-/* @return container with data from raw and closed with \\0 */
+/* create string from raw char pointer */
+/* @param raw char array */
+/* @return string with data from raw and closed with \\0 */
 String* cs_create(const char* raw);
 
 /* string length getter */
-/* @param self string container */
+/* @param self string */
 /* @return copy of length field */
 size_t cs_length(const String* self);
 
 /* string capacity getter */
-/* @param self string container */
+/* @param self string */
 /* @return copy of capacity field */
 size_t cs_capacity(const String* self);
 
 /* string text getter */
-/* @param self string container */
+/* @param self string */
 /* @return readonly representation of raw text field */
 const char* cs_raw(const String* self);
 
 /* free string's memory */
-/* @param self string container */
+/* @param self string */
 void cs_free(String* self);
 
 /* for access to raw field by index */
-/* @param self string container */
+/* @param self string */
 /* @param index index in char array */
-/* @return char by index of -1, if index was out of range */
+/* @return char by index or -1, if index was out of range */
 char cs_get(const String* self, size_t index);
 
 /* for access to raw field by index */
-/* @param self string container */
+/* @param self string */
 /* @param item character */
 /* @param index index in char array */
 void cs_set(const String* self, char item, size_t index);
 
 /* copy of string */
-/* @param self string container */
+/* @param self string */
 /* @return cloned string */
 String* cs_clone(const String* self);
 
-/* compares string with raw char* */
+/* compares string text field with raw char array */
 /* @param str string */
 /* @param value char array */
 /* @param ignore_case ignore args case */
-/* @return size_t that indicates arg2 position in the sort order */
+/* @return integer that indicates args position in the sort order */
 int64_t cs_compare(const String* str, const char* raw, bool ignore_case);
 
-/* concatenates string with raw char* */
+/* concatenates string with raw char array */
 /* @param str string */
 /* @param value char array */
 void cs_concat(String* str, const char* value);
@@ -73,7 +73,7 @@ void cs_append(String* str, char item);
 /* check if string contains substring */
 /* @param str string */
 /* @param value char array to seek */
-/* @return true if the value occurs within this string, otherwise false. */
+/* @return true if the value occurs within this string, otherwise false */
 bool cs_contains(String* str, const char* value);
 
 /* find the index of substring in string */
